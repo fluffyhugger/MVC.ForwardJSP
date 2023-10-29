cada0b65a24dbec705a7f77c53ed83ad394a94b9
Sirapob <82280678+fluffyhugger@users.noreply.github.com>
2023-10-30 00:12:39 +0700

    Add files via upload

---
+++ beforeExam1/src/main/java/com/example/beforeexam1/SubjectListServlet.java
@@ -0,0 +1,45 @@
+package com.example.beforeexam1;
+
+import com.example.beforeexam1.entities.Subject;
+import com.example.beforeexam1.repositories.SubjectRepository;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "SubjectListServlet", value = "/subjects")
+
+public class SubjectListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        SubjectRepository sr = new SubjectRepository();
+        List<Subject> subjectList = sr.findAll();
+        request.setAttribute("subjects",subjectList); //เอาค่าของ Subject ไปใส่ใน Request ส่งให้ JSP
+        request.getRequestDispatcher("/subjects_list.jsp").forward(request,response);
+//        PrintWriter out = response.getWriter();
+//        out.println("<html><body>");
+//        out.println("<h1> Subject List :: </h1>");
+//        out.println("<table>");
+//        out.println("<tr>");
+//        out.println("<td>Subject Id</td><td>Subject Title</td<td>Credit</td>");
+//        for(Subject subject : sr.findAll()){
+//            out.println("<tr>");
+//            out.println("<td>" + subject.getId() +"</td>");
+//            out.println("<td>" + subject.getTitle() +"</td>");
+//            out.println("<td>" + subject.getCredit() +"</td>");
+//        }
+//        out.println("</table>");
+//        out.println("</body></html>");
+    }
+
+    @Override
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
---
+++ beforeExam1/target/beforeExam1-1.0-SNAPSHOT/subjects_list.jsp
@@ -0,0 +1,42 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: sirapob
+  Date: 29/10/2023 AD
+  Time: 23:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>Subject List</title>
+</head>
+<body>
+    <div class="flex-content" style="margin: 50px">
+        <div style="background-color: beige;color: darkgoldenrod">
+            <h1>Subject List::</h1>
+        </div>
+        <div>
+            <table>
+                <tr>
+                    <td>No </td>
+                    <td>Subject Code</td>
+                    <td>Title</td>
+                    <td>Credit</td>
+                    <td>Select</td>
+                </tr>
+                <c:forEach items="${subjects}" var="subject" varStatus="vs">
+                    <tr>
+                        <td>${vs.count}</td>
+                        <td>${subject.id}</td>
+                        <td>${subject.title}</td>
+                        <td>${subject.credit}</td>
+                        <td><input type="checkbox" value="${subject.id}" name="subjects"></td>
+                    </tr>
+                </c:forEach>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
